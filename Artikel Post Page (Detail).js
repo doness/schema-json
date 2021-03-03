8a3e1756d4c16545b80c94b34e08c23676776dd5
Doness <doness@users.noreply.github.com>
2021-03-03 11:05:17 +0700

    Create Artikel Post Page (Detail).js

---
+++ Artikel Post Page (Detail).js
@@ -0,0 +1,26 @@
+<script type="application/ld+json">
+	{
+	  "@context": "https://schema.org",
+	  "@type": "Article",
+	  "mainEntityOfPage": {
+		"@type": "WebPage",
+		"@id": "https://www.kalcare.com/artikel/makanan-yang-aman-dikonsumsi-untuk-malam-hari"
+	  },
+	  "headline": "Makanan Yang Aman Dikonsumsi Untuk Malam Hari",
+	  "image": "https://articles.kalcare.com/wp-content/uploads/Makanan-Yang-Baik-Dikonsumsi-Pada-Malam-Hari.jpg",  
+	  "author": {
+		"@type": "Organization",
+		"name": "KALcare"
+	  },  
+	  "publisher": {
+		"@type": "Organization",
+		"name": "KALcare",
+		"logo": {
+		  "@type": "ImageObject",
+		  "url": "ttps://www.kalcare.com/static/img/kalcare-with-tagline.svg"
+		}
+	  },
+	  "datePublished": "2021-02-12",
+	  "dateModified": "2021-02-12"
+	}
+</script>
